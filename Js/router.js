a4a4f9ec6f0b6544f51bf45e5e406ff4b9a68d02
Shishir Kharel <shishir167@gmail.com>
2015-09-11 16:37:06 -0400

    Used a different way for routing

--- Js/router.js
+++ Js/router.js
@@ -26,8 +26,42 @@ define([
         }
     });
 
+
+
     var initialize = function() {
+        Backbone.history.start({pushState: false,
+        root:'/'});
         var app_router = new AppRouter();
+
+        // Determine browser device type.
+        var isMobileWebBrowser = _mobileDetection.IsMobileWebBrowser();
+       // debugger;
+        console.log(_mobileDetection.IsNativeApp());
+
+        if (isMobileWebBrowser === true) {
+            $("#content").load("Templates/mobileMain.html", function() {
+                //code to toggle navbar in mobile
+                $('[data-toggle="showLeftPush"]').click(function() {
+                    $('body').toggleClass('cbp-spmenu-push-toleft');
+                    $('#cbp-spmenu-s2').toggleClass('cbp-spmenu-open');
+                });
+
+                $('[data-toggle="showRightPush"]').click(function() {
+                    $('body').toggleClass('cbp-spmenu-push-toright');
+                    $('#cbp-spmenu-s1').toggleClass('cbp-spmenu-open');
+                });
+
+
+                //$('#firstTemplate').empty().append('<h1>Hello Mobile User</h1>');
+
+            });
+        } else {
+            $("#content").load("Templates/desktopMain.html", function() {
+                $('#content').attr('style', 'padding-left: 250px !important'); //Moving body for the static sidebar in desktop
+                $('#firstTemplate').empty().append('<h1>Hello Desktop User</h1>');
+            });
+
+        }
 
         app_router.on("route:showHome", function() {
             console.log("got home");
@@ -75,36 +109,34 @@ define([
             });
         });
 
-        Backbone.history.start();
 
-        // Determine browser device type.
-        var isMobileWebBrowser = _mobileDetection.IsMobileWebBrowser();
-        console.log(_mobileDetection.IsNativeApp());
+        // All navigation that is relative should be passed through the navigate
+        // method, to be processed by the router. If the link has a `data-bypass`
+        // attribute, bypass the delegation completely.
+        $(document).on('click', 'a[href]:not([data-bypass])', function (evt) {
+        // Get the absolute anchor href.
+        var href = { prop: $(this).prop('href'), attr: $(this).attr('href') };
+        // Get the absolute root.
+        var root = location.protocol + '//' + location.host ;
 
-        if (isMobileWebBrowser === true) {
-            $("#content").load("Templates/mobileMain.html", function() {
-                //code to toggle navbar in mobile
-                $('[data-toggle="showLeftPush"]').click(function() {
-                    $('body').toggleClass('cbp-spmenu-push-toleft');
-                    $('#cbp-spmenu-s2').toggleClass('cbp-spmenu-open');
-                });
+        // Ensure the root is part of the anchor href, meaning it's relative.
+        if (href.prop.slice(0, root.length) === root) {
+          // Stop the default event to ensure the link will not cause a page
+          // refresh.
+          evt.preventDefault();
 
-                $('[data-toggle="showRightPush"]').click(function() {
-                    $('body').toggleClass('cbp-spmenu-push-toright');
-                    $('#cbp-spmenu-s1').toggleClass('cbp-spmenu-open');
-                });
-
-
-                $('#firstTemplate').empty().append('<h1>Hello Mobile User</h1>');
-
-            });
-        } else {
-            $("#content").load("Templates/desktopMain.html", function() {
-                $('#content').attr('style', 'padding-left: 250px !important'); //Moving body for the static sidebar in desktop
-                $('#firstTemplate').empty().append('<h1>Hello Desktop User</h1>');
-            });
-
+          // `Backbone.history.navigate` is sufficient for all Routers and will
+          // trigger the correct events. The Router's internal `navigate` method
+          // calls this anyways.  The fragment is sliced from the root.
+          Backbone.history.navigate(href.attr, true);
         }
+        });
+
+        $(document).on('click', 'a[href][target=\'_blank\'][data-bypass]', function (evt) {
+            evt.preventDefault();
+            // fix for non http urls not opening (maps:) for example -- https://issues.apache.org/jira/browse/CB-5521
+            window.open($(evt.currentTarget).attr('href'), $(evt.currentTarget).attr('href').indexOf('http') === 0 ? '_system' : '_self', 'location=yes');
+        });
 
     };
     return {
