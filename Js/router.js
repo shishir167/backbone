47dc2c47a9701f20e79cb7d67aab20858a0e5060
Shishir Kharel <shishir167@gmail.com>
2015-09-14 11:23:48 -0400

    Changed router to support pushstate

--- Js/router.js
+++ Js/router.js
@@ -29,15 +29,11 @@ define([
 
 
     var initialize = function() {
-        Backbone.history.start({pushState: false,
-        root:'/'});
-        var app_router = new AppRouter();
 
         // Determine browser device type.
         var isMobileWebBrowser = _mobileDetection.IsMobileWebBrowser();
-       // debugger;
+        // debugger;
         console.log(_mobileDetection.IsNativeApp());
-
         if (isMobileWebBrowser === true) {
             $("#content").load("Templates/mobileMain.html", function() {
                 //code to toggle navbar in mobile
@@ -63,6 +59,14 @@ define([
 
         }
 
+
+        Backbone.history.start({
+            pushState: true,
+            root: '/'
+        });
+        var app_router = new AppRouter();
+
+
         app_router.on("route:showHome", function() {
             console.log("got home");
             $('#firstTemplate').empty().append('<h1>Hello Desktop User</h1>');
@@ -76,9 +80,10 @@ define([
         });
 
         app_router.on("route:showStep1", function() {
-            require(['views/step1View'], function(Step2View) {
+            console.log("step1");
+            require(['views/step1View'], function(Step1View) {
                 $('#firstTemplate').empty();
-                Step2View.initialize($('#firstTemplate'));
+                Step1View.initialize($('#firstTemplate'));
             });
         });
 
@@ -113,26 +118,29 @@ define([
         // All navigation that is relative should be passed through the navigate
         // method, to be processed by the router. If the link has a `data-bypass`
         // attribute, bypass the delegation completely.
-        $(document).on('click', 'a[href]:not([data-bypass])', function (evt) {
-        // Get the absolute anchor href.
-        var href = { prop: $(this).prop('href'), attr: $(this).attr('href') };
-        // Get the absolute root.
-        var root = location.protocol + '//' + location.host ;
+        $(document).on('click', 'a[href]:not([data-bypass])', function(evt) {
+            // Get the absolute anchor href.
+            var href = {
+                prop: $(this).prop('href'),
+                attr: $(this).attr('href')
+            };
+            // Get the absolute root.
+            var root = location.protocol + '//' + location.host;
 
-        // Ensure the root is part of the anchor href, meaning it's relative.
-        if (href.prop.slice(0, root.length) === root) {
-          // Stop the default event to ensure the link will not cause a page
-          // refresh.
-          evt.preventDefault();
+            // Ensure the root is part of the anchor href, meaning it's relative.
+            if (href.prop.slice(0, root.length) === root) {
+                // Stop the default event to ensure the link will not cause a page
+                // refresh.
+                evt.preventDefault();
 
-          // `Backbone.history.navigate` is sufficient for all Routers and will
-          // trigger the correct events. The Router's internal `navigate` method
-          // calls this anyways.  The fragment is sliced from the root.
-          Backbone.history.navigate(href.attr, true);
-        }
+                // `Backbone.history.navigate` is sufficient for all Routers and will
+                // trigger the correct events. The Router's internal `navigate` method
+                // calls this anyways.  The fragment is sliced from the root.
+                Backbone.history.navigate(href.attr, true);
+            }
         });
 
-        $(document).on('click', 'a[href][target=\'_blank\'][data-bypass]', function (evt) {
+        $(document).on('click', 'a[href][target=\'_blank\'][data-bypass]', function(evt) {
             evt.preventDefault();
             // fix for non http urls not opening (maps:) for example -- https://issues.apache.org/jira/browse/CB-5521
             window.open($(evt.currentTarget).attr('href'), $(evt.currentTarget).attr('href').indexOf('http') === 0 ? '_system' : '_self', 'location=yes');
